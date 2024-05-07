df73af1f82412419c5ab965d3eaf58e6dce04e0a
den <viewsonic352@gmail.com>
2024-05-07 18:32:13 +0300

    fihish commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [quote, setQuote] = useState(null)
+	const [author, setAuthor] = useState(null);
+
+	useEffect(() => {
+		fetch('http://api.quotable.io/random')
+			.then(res => res.json())
+			.then(
+				(quote) => {
+					setQuote(quote.content)
+					setAuthor(quote.author)
+				}
+			)
+	},[])
+
+	let fetchNewQuote = () => {
+		fetch('http://api.quotable.io/random')
+			.then(res => res.json())
+			.then(
+				(quote) => {
+					setQuote(quote.content)
+					setAuthor(quote.author)
+				}
+			)
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className='App-newWindow'>
+					<h2>{quote}</h2>
+					<h3>{author}</h3>
+
+					<button className='button'  onClick={fetchNewQuote}>New</button>
+				</div>
+				<div className='App-comunication'>
+					<a className='App-info' href="https://github.com/DenisMov">© Denys Movchan</a>
+				</div>
+			</header>
+		</div>
+	);
 }
+
 
 export default App;
